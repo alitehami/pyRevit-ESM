89395056513f373ac76592645b218287db72b760
ali-tehami <ali.tehami@yahoo.com>
2020-07-13 03:22:53 +1000

    first working prototype

---
+++ TOOLS..md
@@ -0,0 +1,23 @@
+# Dev Tools & Libraries
+* [JSON-EDITOR Repo](https://github.com/json-editor/json-editor#json-editor)
+* [JSON-EDITOR Interactive Demo](https://json-editor.github.io/json-editor/)
+* [JSON-EDITOR Play Ground](https://pmk65.github.io/jedemov2/dist/demo.html)
+* [JSON Schemas Generator](https://niehausbert.gitlab.io/JSON2Schema/)
+* [YAML.js - a json to yaml convertor](https://github.com/jeremyfa/yaml.js)
+* [pyRevit Extension Metadata docs](https://www.notion.so/Extension-Metadata-aaab45d8b4854e289faa6a6fd47c1c60#7059190b63ab4008b8a1c9e19f943e61)
+```
+Github repo:
+https://github.com/jeremyfa/yaml.js
+
+Mention link:
+https://www.quora.com/How-can-I-convert-YAML-into-JSON-in-JavaScript-frontend-code-not-Node
+
+Nested Json parsing to Yaml:
+https://github.com/jeremyfa/yaml.js/issues/59
+```
+
+# Ideas
+* accept yaml or json file types
+* export as yaml or json
+* extend tools to create settings for individual tools (yaml files living next to each tool)
+* extend tools to build the UI arrangment yaml file
--- js/app.js
+++ js/app.js
@@ -14,10 +14,19 @@ document
   });
 
 let download_click = document.getElementById("download_click");
+let download_json = document.getElementById("submit_json");
+let download_yaml = document.getElementById("submit_yaml");
+
+
+
 let download = document.createElement("a");
+
+
+
 download_click.addEventListener("click", function () {
+  holder = editor.getValue();
   // download_click.preventDefault();
-  console.log("downloading...")
+  console.log("downloading...");
   let data =
     "data:text/json;charset=utf-8," +
     encodeURIComponent(JSON.stringify(holder));
@@ -25,3 +34,54 @@ download_click.addEventListener("click", function () {
   download.setAttribute("download", "updated_extension.json");
   download.click();
 });
+
+
+download_json.addEventListener("click", function () {
+  // download_json.preventDefault();
+  holder = JSON.stringify(editor.getValue());
+  console.log("downloading JSON..." + "\n" + holder);
+  let data =
+    "data:text/json;charset=utf-8," +
+    encodeURIComponent(holder);
+  download.setAttribute("href", data);
+  download.setAttribute("download", "updated_extension.json");
+  download.click();
+});
+
+
+download_yaml.addEventListener("click", function () {
+  // download_yaml.preventDefault();
+  holder = JSONtoYAML(editor.getValue());
+  console.log("downloading YAML..." + "\n" + holder);
+  let data =
+    "data:text/yaml;charset=utf-8," +
+    encodeURIComponent(holder);
+  download.setAttribute("href", data);
+  download.setAttribute("download", "updated_extension.yaml");
+  download.click();
+});
+
+
+/* HELPER FUNCTIONS */
+
+// yaml.js / JSON TO YAML converter library >> available under ../lib/yaml.js
+// https://github.com/jeremyfa/yaml.js
+// https://www.quora.com/How-can-I-convert-YAML-into-JSON-in-JavaScript-frontend-code-not-Node
+// json to yaml
+function JSONtoYAML(json) {
+  let obj = null;
+  if (typeof json === "object") {
+    obj = json;
+  } else {
+    obj = JSON.parse(json);
+  }
+  let yamlString = YAML.stringify(obj,Infinity,4);
+  return yamlString;
+}
+// yaml to json
+function YAMLtoJSON(yamlString) {
+  let obj = YAML.parse(yamlString);
+  let jsonString = JSON.stringify(obj);
+  let json = JSON.parse(jsonString);
+  return json;
+}
